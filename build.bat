f488a4db888efe86e1ddcafa3b56955a4e1ac347
Bogumił Chojnowski <bogumil.chojnowski@gmail.com>
2022-07-26 00:45:21 +0200

    Update build.bat
    added missing files

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 rem make ZIP archive
-tar.exe -a -c -f FS22_CropRotation.zip main.lua CropRotation.lua CropRotationData.lua modDesc.xml modIcon.dds
+tar.exe -a -c -f FS22_CropRotation.zip data translations CropRotation.lua CropRotationData.lua main.lua modDesc.xml modIcon.dds
 
 rem copy ZIP to FS22 mods folder
 xcopy /b/v/y FS22_CropRotation.zip "%userprofile%\Documents\My Games\FarmingSimulator2022\mods"
