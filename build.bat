9effd40063062042080478f609e6e87870974364
Bogumił Chojnowski <bogumil.chojnowski@gmail.com>
2023-12-07 23:48:35 +0100

    Fix HelpLine manager
    Invalid function signature caused help line manager to stop reading subsequent help chapters from other mods.
    
    Closes #39 #37

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ tar.exe -a -c -f FS22_CropRotation.zip^
  utils^
  maps^
  translations^
- utils^
  CropRotation.lua^
  CropRotation.xml^
  CropRotationData.lua^
@@ -14,8 +13,9 @@ tar.exe -a -c -f FS22_CropRotation.zip^
  icon_cropRotation.dds^
  modDesc.xml
 
+
 rem copy ZIP to FS22 mods folder
-xcopy /b/v/y FS22_CropRotation.zip "D:\Users\Bodzio\Documents\My Games\FarmingSimulator2022\mods"
+rem xcopy /b/v/y FS22_CropRotation.zip "D:\Users\Bodzio\Documents\My Games\FarmingSimulator2022\mods"
 
 rem make update mod as well
 rem copy /b/v/y FS22_CropRotation.zip FS22_CropRotation_update.zip
